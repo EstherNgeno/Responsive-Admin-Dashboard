b2217cf337f1313b17b57567c494a6887ee01e92
EstherNgeno <estherngeno8@gmail.com>
2025-04-23 14:00:32 +0300

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Responsive-Admin-Dashboard
+
+This is a practice project to sharpen my HTML, CSS and JavaScript skills.
+I was able to implement a completely responsive admin dashboard with light and dark mode functionality.
---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "12345",
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "LARVENDER KF102 Drone",
+        productNumber: "12346",
+        paymentStatus: "Refunded",
+        shipping: "Declined",
+    },
+    {
+        productName: "Ruko F11 Pro Drone",
+        productNumber: "12347",
+        paymentStatus: "Due",
+        shipping: "Pending",
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: "12348",
+        paymentStatus: "Paid",
+        shipping: "Delivered",
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: "12349",
+        paymentStatus: "Paid",
+        shipping: "Delivered",
+    },
+    
+]
